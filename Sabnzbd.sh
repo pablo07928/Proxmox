9dea1f64fccb0af1c99679599d2cdefe7c589397
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 15:42:27 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -24,7 +24,7 @@ pct exec $current_lxc_id mv /root/.sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.in
 echo "Copying sabnzbd.ini to container $current_lxc_id..."
 pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.ini_new
 pct exec $current_lxc_id cp /root/.sabnzbd/sabnzbd.ini_new /root/.sabnzbd/sabnzbd.ini
-
+sleep 20
 # Install iptables in the container
 echo "Installing iptables in container $current_lxc_id..."
 pct exec $current_lxc_id apt install iptables 
@@ -32,15 +32,17 @@ pct exec $current_lxc_id apt install iptables
 # Add iptables rule to redirect port 80 to port 8080
 echo "Adding iptables rule for port redirection in container $current_lxc_id..."
 pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-
+sleep 20
 # Start sabnzbd service
 echo "Starting sabnzbd service in container $current_lxc_id..."
 pct exec $current_lxc_id systemctl start sabnzbd
 
-# Reboot the container
-echo "Rebooting container $current_lxc_id..."
-pct exec $current_lxc_id reboot now
+
 
 # Remove the currentid.sh script
 echo "Removing /etc/pve/lxc/currentid.sh..."
 rm /etc/pve/lxc/currentid.sh
+
+# Reboot the container
+echo "Rebooting container $current_lxc_id..."
+#pct exec $current_lxc_id reboot now
