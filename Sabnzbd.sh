7d3047cae9dc6e201a455e3f0b82588908c48cd5
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 15:01:54 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/ct/sabnzbd.sh)"
+
+#bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/ct/sabnzbd.sh)"
 
 bash -c "$(wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh)"
 
