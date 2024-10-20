f90ba576a125b359ac31c8009c96f04665402497
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 14:22:39 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -5,7 +5,7 @@ bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/ct/sabnzbd.sh)"
 bash -c "$(wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh)"
 
 systemctl stop sabnzbd
-/media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini
+cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini
 apt install iptables -y
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 systemctl start sabnzbd
