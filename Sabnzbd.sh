5ce98c39f429198c14a81d3911d59f4d09d6674b
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 16:27:45 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
+FILE1="/tmp/prevms.txt"
+FILE2="/tmp/postvms.txt"
 
+pct list >>/tmp/prevms.txt
+
+bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/ct/sabnzbd.sh)"
 # Download and execute the AddSharestoLXC.sh script
+
+pct list >>/tmp/postvms.txt
+
+#!/bin/bash
+
+# Define the input files
+# Check if the input files exist
+if [[ ! -f "$FILE1" ]]; then
+    echo "Error: $FILE1 does not exist."
+    exit 1
+fi
+
+if [[ ! -f "$FILE2" ]]; then
+    echo "Error: $FILE2 does not exist."
+    exit 1
+fi
+
+# Extract the VMID column from both files and sort them
+awk 'NR>1 {print $1}' "$FILE1" | sort > sorted_file1_vmids.txt
+awk 'NR>1 {print $1}' "$FILE2" | sort > sorted_file2_vmids.txt
+
+# Use comm to find extra VM IDs in the second file
+comm -13 sorted_file1_vmids.txt sorted_file2_vmids.txt>> /tmp/difference
+
+# Clean up temporary files
+rm sorted_file1_vmids.txt sorted_file2_vmids.txt
+
+
 bash -c "$(wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh)"
+
+
 
 # Load the current LXC ID
 echo "Loading source...v3"
@@ -38,6 +73,7 @@ pct exec $current_lxc_id pct exec $current_lxc_id -- bash -c "iptables -t nat -A
 sleep 60
 # Start sabnzbd service
 echo "Starting sabnzbd service in container $current_lxc_id..."
+pct exec $current_lxc_id pct exec $current_lxc_id -- bash -c "systemctl daemon-reload"
 pct exec $current_lxc_id systemctl start sabnzbd
 
 
