67821e0cfa97d5f7a277976d16761f541d76f176
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 13:02:44 -0400

    Create AddSharestoLXC.sh

---
+++ AddSharestoLXC.sh
@@ -0,0 +1,14 @@
+function prompt_lxc_ID {
+    id=$(whiptail --inputbox "Please enter the LXC ID:" 8 39 --title "LXC Input" --cancel-button Exit-Script 3>&1 1>&2 2>&3)
+        if [ $? -ne 0 ]; then
+            echo "Operation cancelled. Exiting..."
+            exit 1
+        else
+            while [ -z "$user" ]; do
+            whiptail --msgbox "LXC cannot be blank. Please try again." 8 39 --title "Input Error"
+            prompt_lxc_ID
+        done
+    fi
+}
+
+{ echo 'mp0: /media/amedia/,mp=/media/amedia' ; } | tee -a /etc/pve/lxc/$id.conf
