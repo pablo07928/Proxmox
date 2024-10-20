9dd0e363277de0241d89bb572e8854071043f63a
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 15:18:24 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-
-#bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/ct/sabnzbd.sh)"
-
+# Download and execute the AddSharestoLXC.sh script
 bash -c "$(wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh)"
-echo "load source"
-source /etc/pve/lxc/currentid.sh
-echo "the current id is:"$current_lxc_id
-echo "pct exec $current_lxc_id systemctl stop sabnzbd"
-pct exec $current_lxc_id systemctl stop sabnzbd
-echo " pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini"
-pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini
-echo "pct exec $current_lxc_id apt install iptables -y"
-pct exec $current_lxc_id apt install iptables -y
-echo "pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080"
-pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-pct exec $current_lxc_id systemctl start sabnzbd
-pct exec $current_lxc_id reboot now
-rm /etc/pve/lxc/currentid.sh
 
+# Load the current LXC ID
+echo "Loading source..."
+source /etc/pve/lxc/currentid.sh
+
+# Display the current LXC ID
+echo "The current LXC ID is: $current_lxc_id"
+
+# Stop sabnzbd service
+echo "Stopping sabnzbd service in container $current_lxc_id..."
+pct exec $current_lxc_id systemctl stop sabnzbd
+
+# Copy sabnzbd configuration file
+echo "Copying sabnzbd.ini to container $current_lxc_id..."
+pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini
+
+# Install iptables in the container
+echo "Installing iptables in container $current_lxc_id..."
+pct exec $current_lxc_id apt install iptables -y
+
+# Add iptables rule to redirect port 80 to port 8080
+echo "Adding iptables rule for port redirection in container $current_lxc_id..."
+pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+# Start sabnzbd service
+echo "Starting sabnzbd service in container $current_lxc_id..."
+pct exec $current_lxc_id systemctl start sabnzbd
+
+# Reboot the container
+echo "Rebooting container $current_lxc_id..."
+pct exec $current_lxc_id reboot now
+
+# Remove the currentid.sh script
+echo "Removing /etc/pve/lxc/currentid.sh..."
+rm /etc/pve/lxc/currentid.sh
