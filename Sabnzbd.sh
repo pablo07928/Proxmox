7127ab5e3332cfb10f90865765781a9d559322e8
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-20 15:15:36 -0400

    Update Sabnzbd.sh

--- Sabnzbd.sh
+++ Sabnzbd.sh
@@ -7,9 +7,13 @@ bash -c "$(wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharest
 echo "load source"
 source /etc/pve/lxc/currentid.sh
 echo "the current id is:"$current_lxc_id
+echo "pct exec $current_lxc_id systemctl stop sabnzbd"
 pct exec $current_lxc_id systemctl stop sabnzbd
+echo " pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini"
 pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/sabnzbd/sabnzbd.ini
+echo "pct exec $current_lxc_id apt install iptables -y"
 pct exec $current_lxc_id apt install iptables -y
+echo "pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080"
 pct exec $current_lxc_id iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 pct exec $current_lxc_id systemctl start sabnzbd
 pct exec $current_lxc_id reboot now
