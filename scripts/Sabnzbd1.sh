cc93d986245e0e9ea3d8976367848fdd26394bb7
pablo07928 <136869122+pablo07928@users.noreply.github.com>
2024-10-21 07:38:34 -0400

    Update Sabnzbd1.sh

--- scripts/Sabnzbd1.sh
+++ scripts/Sabnzbd1.sh
@@ -43,48 +43,48 @@ current_lxc_id=(find_container_ID $FILE1 $FILE2)
 
 msg_ok " Container id is: $current_lxc_id"
 
-wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh > $temp_root/AddSharestoLXC.sh
-bash -c "chmod +x $temp_root/AddSharestoLXC.sh"
-bash -c "$temp_root/AddSharestoLXC.sh $current_lxc_id"
+# wget -qLO - https://github.com/pablo07928/Proxmox/raw/main/AddSharestoLXC.sh > $temp_root/AddSharestoLXC.sh
+# bash -c "chmod +x $temp_root/AddSharestoLXC.sh"
+# bash -c "$temp_root/AddSharestoLXC.sh $current_lxc_id"
 
-echo "Rebooting serverwith id : $current_lxc_id.. pausing for 60 seconds"
-pct exec $current_lxc_id reboot now
-sleep 60
-# Display the current LXC ID
-
-
-# Stop sabnzbd service
-echo "Stopping sabnzbd service in container $current_lxc_id...pct exec $current_lxc_id systemctl stop sabnzbd"
-pct exec $current_lxc_id systemctl stop sabnzbd
-
-sleep 15
-# Copy sabnzbd configuration file
-
-echo "renaming sabnzbd.ini to container $current_lxc_id..."
-pct exec $current_lxc_id rm /root/.sabnzbd/sabnzbd.ini_orig
-pct exec $current_lxc_id mv /root/.sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.ini_orig
-
-echo "Copying sabnzbd.ini to container $current_lxc_id..."
-pct exec $current_lxc_id rm /root/.sabnzbd/sabnzbd.ini_new
-pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.ini_new
-pct exec $current_lxc_id cp /root/.sabnzbd/sabnzbd.ini_new /root/.sabnzbd/sabnzbd.ini
-sleep 20
-
-pct exec $current_lxc_id -- bash -c "sed -i 's|ExecStart=python3 SABnzbd.py -s 0.0.0.0:7777|ExecStart=python3 SABnzbd.py -s 0.0.0.0|' /etc/systemd/system/sabnzbd.service"
-# Install iptables in the container
-echo "Installing iptables in container $current_lxc_id..."
-pct exec $current_lxc_id -- bash -c "apt install iptables -y"
-
-# Add iptables rule to redirect port 80 to port 8080
-echo "Adding iptables rule for port redirection in container $current_lxc_id..."
-pct exec $current_lxc_id -- bash -c "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080"
-
-# Start sabnzbd service
-echo "Starting sabnzbd service in container $current_lxc_id..."
-pct exec $current_lxc_id -- bash -c "systemctl daemon-reload"
-pct exec $current_lxc_id systemctl start sabnzbd
+# echo "Rebooting serverwith id : $current_lxc_id.. pausing for 60 seconds"
+# pct exec $current_lxc_id reboot now
+# sleep 60
+# # Display the current LXC ID
 
 
-# Reboot the container
-echo "Rebooting container $current_lxc_id..."
-pct exec $current_lxc_id reboot now
+# # Stop sabnzbd service
+# echo "Stopping sabnzbd service in container $current_lxc_id...pct exec $current_lxc_id systemctl stop sabnzbd"
+# pct exec $current_lxc_id systemctl stop sabnzbd
+
+# sleep 15
+# # Copy sabnzbd configuration file
+
+# echo "renaming sabnzbd.ini to container $current_lxc_id..."
+# pct exec $current_lxc_id rm /root/.sabnzbd/sabnzbd.ini_orig
+# pct exec $current_lxc_id mv /root/.sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.ini_orig
+
+# echo "Copying sabnzbd.ini to container $current_lxc_id..."
+# pct exec $current_lxc_id rm /root/.sabnzbd/sabnzbd.ini_new
+# pct exec $current_lxc_id cp /media/scripts/sabnzbd/sabnzbd.ini /root/.sabnzbd/sabnzbd.ini_new
+# pct exec $current_lxc_id cp /root/.sabnzbd/sabnzbd.ini_new /root/.sabnzbd/sabnzbd.ini
+# sleep 20
+
+# pct exec $current_lxc_id -- bash -c "sed -i 's|ExecStart=python3 SABnzbd.py -s 0.0.0.0:7777|ExecStart=python3 SABnzbd.py -s 0.0.0.0|' /etc/systemd/system/sabnzbd.service"
+# # Install iptables in the container
+# echo "Installing iptables in container $current_lxc_id..."
+# pct exec $current_lxc_id -- bash -c "apt install iptables -y"
+
+# # Add iptables rule to redirect port 80 to port 8080
+# echo "Adding iptables rule for port redirection in container $current_lxc_id..."
+# pct exec $current_lxc_id -- bash -c "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080"
+
+# # Start sabnzbd service
+# echo "Starting sabnzbd service in container $current_lxc_id..."
+# pct exec $current_lxc_id -- bash -c "systemctl daemon-reload"
+# pct exec $current_lxc_id systemctl start sabnzbd
+
+
+# # Reboot the container
+# echo "Rebooting container $current_lxc_id..."
+# pct exec $current_lxc_id reboot now
